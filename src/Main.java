5538ea6fc3985641f867b853fb18e3417078b88a
Denis Orlov <spartak20002000@gmail.com>
2024-07-16 23:26:18 +0300

    bonusnew1

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,21 @@
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
 
         int firstData = 100;
-        int Replenishment = 1100;
+        int replenishment = 1100;
 
         int bonus;
-        if (Replenishment > 1000)
-        {
-            bonus = (Replenishment / 100) * 1;
+        if (replenishment > 1000) {
+            bonus = (replenishment / 100) * 1;
 
-        }    else {
+        } else {
             bonus = 0;
         }
-        int totalBalance = firstData + Replenishment + bonus;
+        int totalBalance = firstData + replenishment + bonus;
 
         System.out.println("Итоговая сумма на счету:" + totalBalance);
         System.out.println("Из них бонусные:" + bonus);
-
-
-
-
-
 
 
     }
